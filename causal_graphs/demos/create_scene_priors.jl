b1cd43f83c124cb1db2138e0058249b4ebdc39ce
tristanyates <tristan.yates@yale.edu>
2020-06-03 11:56:38 -0400

    _Gen scenarios allow you to make random decisions in gen

--- causal_graphs/demos/create_scene_priors.jl
+++ causal_graphs/demos/create_scene_priors.jl
@@ -1,11 +1,15 @@
-#!/usr/bin/env julia
+#!/usr/bin/env
 # # Generate the random variables that will be inputs to the initial scene
 # Starting with a occlusion event
 # TY 05/13/2020
+# add containment
+# TY 06/01/2020
 
 # Need to import
-#import Pkg; Pkg.add("Gen")
-#import Pkg; Pkg.add("JSON")
+#import Pkg
+#Pkg.add("General")
+#Pkg.add("Gen")
+#Pkg.add("JSON")
 
 # Load the necessary things
 using Gen
@@ -17,8 +21,8 @@ trace_number=ARGS[2]
 #println(scene_type)
 #println(trace_number)
 
-# Define our generative model
-@gen function generate_occlusion_scene(x::Int) # how many objects will there be? what scene type is it? 
+# Define occlusion generative model
+@gen function generate_occlusion_scene(x::Int) # how many objects will there be? 
     
     ## 1. Choose the object type 
     # Is it going to be a ball?
@@ -36,7 +40,7 @@ trace_number=ARGS[2]
         force=@trace(uniform(0.01,0.04), :force) 
     end
     
-    ## 2. Scene-specific things
+    ## 2. Scene-specific things: Occluder
     occl_l=@trace(uniform(0.1,0.4),:occl_length) #(0.1, 0.001, 0.4)  
     occl_w=@trace(uniform(0.0005,0.0001),:occl_width)
     occl_h=@trace(uniform(0.1,0.6),:occl_height) 
@@ -44,11 +48,41 @@ trace_number=ARGS[2]
       
 end;
 
+# Define our generative model
+@gen function generate_containment_scene(x::Int) # how many objects will there be?  
+    
+    ## 1. Choose the object type 
+    # Is it going to be a ball?
+    if @trace(bernoulli(0.5), :is_ball)
+        
+        radius=@trace(uniform(0.005,0.03),:ball_radius)   # 0.02 [m]
+        #force=@trace(uniform(8,13), :force)
+        
+    # Or will it be a plank?
+    else
+        
+        plank_l=@trace(uniform(0.01,0.03),:plank_length) # (.02, .025, .02) [m]
+        plank_w=@trace(uniform(0.01,0.03),:plank_width)
+        plank_h=@trace(uniform(0.01,0.03),:plank_height)
+        force=@trace(uniform(-0.002,-0.01), :force) # NEGATIVE here
+    end
+    
+    ## 2. Scene-specific things: Goblet
+    goblet_h=@trace(uniform(0.05,0.15),:goblet_h) #(0.1, 0.001, 0.4)  
+    goblet_R1=@trace(uniform(0.01,0.06),:goblet_R1)
+    goblet_R2=@trace(uniform(0.01,0.06),:goblet_R2) 
+    goblet_EPS=@trace(uniform(0.001,0.003), :goblet_EPS)
+    
+    ## 3. Scene-specific things: Height and angle of ramp
+    ramp_h=@trace(uniform(-0.05,0.05),:ramp_h)
+    ramp_ang=@trace(uniform(-10,5),:ramp_ang)
+      
+end;
     
 # Function to read the generative trace
 function write_gen_trace(trace,scene_type,save_file)
    
-    #Is it a ball or a plank?
+    #Is it a ball or a plank? (This is always relevant!)
     is_ball=Gen.get_value(Gen.get_choices(trace),:is_ball)
     
     # Make the dictionary depending on the scene type  
@@ -83,7 +117,44 @@ function write_gen_trace(trace,scene_type,save_file)
                 "occluder_lwh" => Dict("length" => occl_l, "width" => occl_w, "height" => occl_h),
                 "occluder_pos" => position
             )
-        end   
+        end  
+        
+    elseif scene_type=="containment_Gen"
+    	
+    	# Get the things just generally relevant to this scene
+        force=Gen.get_value(Gen.get_choices(trace),:force)
+        goblet_h=Gen.get_value(Gen.get_choices(trace),:goblet_h)  
+        goblet_R1=Gen.get_value(Gen.get_choices(trace),:goblet_R1)
+        goblet_R2=Gen.get_value(Gen.get_choices(trace),:goblet_R2) 
+        goblet_EPS=Gen.get_value(Gen.get_choices(trace), :goblet_EPS)
+        ramp_h=Gen.get_value(Gen.get_choices(trace), :ramp_h)
+        ramp_ang=Gen.get_value(Gen.get_choices(trace), :ramp_ang)
+        
+        # Then get the relevant information depending on if its a ball or a plank
+        # And make the trace that will be saved
+        if is_ball
+            radius=Gen.get_value(Gen.get_choices(trace),:ball_radius)
+            
+            trace_dict = Dict("is_ball" => is_ball,
+                "ball_radius" => radius,
+                "force" => force,
+                "goblet" => Dict("height" => goblet_h, "R1" => goblet_R1, "R2" => goblet_R2, "EPS" => goblet_EPS),
+                "ramp" => Dict("height" => ramp_h, "angle" => ramp_ang)
+             )
+            
+        else
+            plank_length=Gen.get_value(Gen.get_choices(trace),:plank_length)
+            plank_width=Gen.get_value(Gen.get_choices(trace),:plank_width)
+            plank_height=Gen.get_value(Gen.get_choices(trace),:plank_height)
+
+            trace_dict = Dict("is_ball" => is_ball, 
+                "plank_lwh" => Dict("length"=>plank_length,"width"=>plank_width,"height"=>plank_height),
+                "force" => force,
+                "goblet" => Dict("height" => goblet_h, "R1" => goblet_R1, "R2" => goblet_R2, "EPS" => goblet_EPS),
+                "ramp" => Dict("height" => ramp_h, "angle" => ramp_ang)
+            )
+        end  
+     
     end
     
     # Put it in a JSON format
@@ -99,11 +170,25 @@ end;
 
 # Simulate based on the scene type
 if scene_type=="occlusion_Gen"
+    
     # Simulate
     trace = Gen.simulate(generate_occlusion_scene, (0,));
+    
+elseif scene_type=="containment_Gen"
+	# Simulate
+    trace = Gen.simulate(generate_containment_scene, (0,));
 end
 
-println(string("Wrote gen trace to gen_jsons/scene_trace_",scene_type,trace_number,".json"))
+# Were you able to define it?
+@isdefined(trace) || println(string("ERROR: Gen function not set up for scene type: ",scene_type))
 
-# Write 
-write_gen_trace(trace,scene_type,string("gen_jsons/scene_trace_",scene_type,trace_number,".json"))
+# If so, save!
+if @isdefined(trace)
+    
+    # talk to them 
+    println(string("Wrote gen trace to gen_jsons/scene_trace_",scene_type,trace_number,".json"))
+
+    # Write it out
+    write_gen_trace(trace,scene_type,string("gen_jsons/scene_trace_",scene_type,trace_number,".json"))
+
+end
--- causal_graphs/scenarios/containment_Gen.py
+++ causal_graphs/scenarios/containment_Gen.py
@@ -1,18 +1,16 @@
 from math import atan, degrees
-
 import math
 import random
-import sys
-import os
 import json
 
 # Which scene trace are you loading in? (Currently doesn't work because need to edit the import_scenario function located in core/scenario.py and how this function is called in demos/import_scenario.py ...)
 # which_trace=sys.argv[1]
 
 # Load in the file that contains the generated items (should be based on a seed input but just default now)
-with open('gen_jsons/scene_trace_occlusion_Gen2.json') as f:
+with open('gen_jsons/scene_trace_containment_Gen0.json') as f:
     trace = json.load(f)
-    
+
+
 DENSITY = 1
 
 if trace["is_ball"]:
@@ -25,43 +23,55 @@ else:
     FLAT_SUPPORT_LWH = (trace['plank_lwh']['length'],trace['plank_lwh']['width'],trace['plank_lwh']['height']) # [m]
     HIGH_PLANK_MASS = FLAT_SUPPORT_LWH[0] * FLAT_SUPPORT_LWH[1] * FLAT_SUPPORT_LWH[2] * DENSITY # [kg]
 
-TOP_TRACK_LWHT = (0.3, 0.025, 0.006, 0.003)  # [m]
 
-# Randomness: size of occluder
-HIGH_PLANK_LWH = (trace["occluder_lwh"]['length'], trace["occluder_lwh"]['width'], trace["occluder_lwh"]['height'])  # [m]
+
+TOP_TRACK_LWHT = (0.5, 0.05, 0.006, 0.003)  # [m]
+BOTTOM_TRACK_LWHT = TOP_TRACK_LWHT  # [m]
+
+HIGH_PLANK_LWH = (0.235, 0.023, 0.008)  # [m]
 HIGH_PLANK_RESTITUTION = 0.8
 
-# Randomness: Ball or Plank
+
+# Randomness: Size of Goblet
+GOBLET_HEIGHT = trace['goblet']['height']  # [m]
+GOBLET_R1 = trace['goblet']['R1']   # [m]
+GOBLET_R2 = trace['goblet']['R2']  # [m]
+GOBLET_EPS = trace['goblet']['EPS']   # [m]
+
+# Randomness: Ball or plank
 if trace["is_ball"]:
     movingObject = {
             'name': "ball",
             'type': "Ball",
             'args': {
                 'radius': BALL_RADIUS,
-                # Randomness: force on object
-                'force': (trace["force"],0,0),
+                'force': (0,0,0),
                 'b_mass': BALL_MASS,
                 'b_restitution': BALL_RESTITUTION
             },
+            'parent': "track",
             'xform': {
-                'value': [-TOP_TRACK_LWHT[0], BALL_RADIUS*4, BALL_RADIUS+.002,
+                'value': [BOTTOM_TRACK_LWHT[0]/2, 0, BALL_RADIUS+.002,
                           0, 0, 0]
             }
     }
 else:
-    movingObject = { 
+    movingObject = {
             'name': "plank",
             'type': "Box",
             'args': {
                 'extents': FLAT_SUPPORT_LWH,
-                # Randomness: force on object
-                'force': (trace["force"],0,0),
+                # Randomness: force on plank
+                'force': (trace['force'],0,0),
                 'b_mass': HIGH_PLANK_MASS,
                 'b_restitution': HIGH_PLANK_RESTITUTION
             },
+            'parent': "track",
             'xform': {
-                'value': [-TOP_TRACK_LWHT[0]/2+.01, 0, FLAT_SUPPORT_LWH[0]/2,
-                    0, 0, 90],
+                'value': [
+                    BOTTOM_TRACK_LWHT[0]/2-.02, 0,  0.028, # Previously used ball radius which isn't defined
+                          0, 0, 60
+                ],
             }
     }
 
@@ -81,22 +91,39 @@ DATA = {
             'args': {
                 'normal': [0, 0, 1],
                 'distance': 0
+            },
+        },
+        {
+            'name': "track",
+            'type': "Track",
+            'args': {
+                'extents': BOTTOM_TRACK_LWHT,
+            },
+            # Randomness: height and angle of ramp
+            'xform': {
+                'value': [0,
+                          BOTTOM_TRACK_LWHT[1]/2+.01,
+                          .2+BOTTOM_TRACK_LWHT[2]/2+.05+trace['ramp']['height'],
+                          0, 0, -5+trace['ramp']['angle']],
             }
         },
         movingObject,
         {
-            'name': "occluder",
-            'type': "Box",
+            'name': "goblet",
+            'type': "Goblet",
             'args': {
-                'extents': HIGH_PLANK_LWH,
-                'force': (0,0,0)
+                'extents': [
+                    GOBLET_HEIGHT,
+                    GOBLET_R1,
+                    GOBLET_R2,
+                    GOBLET_EPS
+                ]
             },
-            # Randomness: position of occluder
+            'parent': "track",
+            # Randomness: position of goblet
             'xform': {
-                'value': [.3, trace["occluder_pos"], .05, 0, 0, 90],
+                'value': [-.35, GOBLET_R1-.02, -.08, 0, 0, 40],
             }
         },
-        
     ],
 }
-
